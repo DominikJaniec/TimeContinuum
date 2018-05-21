2cbd700af35ce18e1ee32b00aa07af448792aba3
Dominik Janiec <dominikjaniec@gmail.com>
2018-05-22 01:13:42 +0200

    Project's Tests

---
+++ TimeContinuumTests/SourceTests.cs
@@ -0,0 +1,14 @@
+using TimeContinuum;
+using Xunit;
+
+namespace TimeContinuumTests
+{
+    public class SourceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(false, nameof(Source));
+        }
+    }
+}
---
+++ TimeContinuumTests/TestComposerTests.cs
@@ -0,0 +1,14 @@
+﻿using TimeContinuum;
+using Xunit;
+
+namespace TimeContinuumTests
+{
+    public class TestComposerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(false, nameof(TestComposer));
+        }
+    }
+}
